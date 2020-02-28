c2784efe01658c18c9756dcecb35e677a345d9f9
adbookerfhsu <58403630+adbookerfhsu@users.noreply.github.com>
2020-02-28 16:52:30 -0600

    fixed JOIN query and created FORM for drop down menu on categories

--- item_list.php
+++ item_list.php
@@ -3,24 +3,25 @@
 
  <!-- display a list of categories -->
  <aside>
-    
-        <!-- display a list of categories -->
-        <h2>Categories</h2>
-        <nav>
-            <ul>
-                <!-- display links for all categories -->
-                <?php foreach($categories as $category) : ?>
-                <li>
-                    <a href="?category_id=<?php echo $category['categoryID']; ?>">
-                        <?php echo $category['categoryName']; ?>
-                    </a>
-                </li>
-                <?php endforeach; ?>
-        </ul>
-        </nav>        
+     <br>
+        <?php if ($categories != NULL) { ?>
+                <form action="." method="get" id="choose_category">
+                <label>Category:</label>
+                <select name="category_id">
+                    <option value="0">View All Categories</option>
+                    <?php foreach ($categories as $category) : ?>
+                        <option value="<?php echo $category['categoryID']; ?>">
+                            <?php echo $category['categoryName']; ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select> 
+                <input type="submit" value="Submit" class="purple button">
+                </form>
+            <?php } ?>
+       
     </aside>    
 
-    
+    <?php if ($items != 0) { ?>
     <h1><?php echo $category_name; ?></h1>
     <table>
             <tr>
@@ -29,12 +30,15 @@
                 <th>Category</th>
                 <th>&nbsp;</th>
             </tr>
-            
+                    
                 <?php foreach ($items as $item) : ?>
                 <tr>
                     <td><?php echo $item['Title']; ?></td>
                     <td><?php echo $item['Description']; ?></td>
-                    <td><?php echo $item['categoryID']; ?></td>
+                <?php if($item['categoryName'] == NULL || $item['categoryName'] == FALSE) { ?> 
+                    <td> None </td> 
+                <?php }else { ?> 
+                    <td> <?php echo $item['categoryName'];?></td><?php }?>
                     <td><form action="." method="post">
                     <input type="hidden" name="action"
                            value="delete_item">
@@ -42,7 +46,7 @@
                            value="<?php echo $item['ItemNum']; ?>">
                     <input type="hidden" name="category_id"
                            value="<?php echo $item['categoryID']; ?>">
-                    <input type="submit" value="Delete">
+                    <input type="submit" value="DELETE" class="red">
                 </form></td>
                 </tr>
                 <?php endforeach; ?>
@@ -52,6 +56,10 @@
             </br>
         
         <p><a href="?action=show_add_form">Click here to add new item</a></p>
+                <?php } else {?>
+                <p>No To Do Items Exist Yet. <a href="?action=show_add_form">Click here to add new item</a></p>
+                <?php }?>
+        <p><a href="index.php?action=list_items">View To Do List</a></p>        
         <p><a href="?action=list_categories">View/Edit Categories</a></p>
     </div>
 </main>
--- model/item_db.php
+++ model/item_db.php
@@ -1,40 +1,35 @@
 <?php
 function get_items_by_category($category_id) {
-    if ($category_id == NULL || $category_id == FALSE) {
         global $db;
-        
-        $query = 'SELECT * FROM todoitems
-                  ORDER BY ItemNum';
+        if ($category_id == NULL || $category_id == FALSE) {
+            $query = 'SELECT t.ItemNum, t.Title, t.Description, c.categoryName
+                       FROM todoitems t LEFT JOIN categories c
+                       ON t.categoryID = c.categoryID
+                       ORDER BY t.ItemNum';
+        } else {
+            $query = 'SELECT t.ItemNUM, t.Title, t.Description, c.categoryName
+                      FROM todoitems t 
+                      LEFT JOIN categories c
+                      ON t.categoryID = c.categoryID
+                      WHERE t.categoryID = :category_id
+                      ORDER BY ItemNum';
+        }            
         $statement = $db->prepare($query);
+        $statement->bindValue('category_id', $category_id);
         $statement->execute();
         $items = $statement->fetchAll();
         $statement->closeCursor();
-       
-        
-    } else {
-        
-        global $db;
-        $query = 'SELECT * FROM todoitems
-              WHERE todoitems.categoryID = :category_id
-              ORDER BY ItemNum';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':category_id', $category_id);
-        $statement->execute();
-        $items = $statement->fetchAll();
-        $statement->closeCursor();
-        
-    }return $items;
+        return $items;
 }
    
 
 function get_item($category_id) {
         global $db;
-        $query = 'SELECT t.Title, c.categoryName
-                  FROM todoitems t  
-                  LEFT OUTER JOIN categories c 
-                  ON t.categoryID = c.categoryID';
-                  
+        $query = 'SELECT *
+                  FROM todoitems  
+                  WHERE ItemNum = :item_num';
         $statement = $db->prepare($query);
+        $statement->bindValue(':item_num', $item_num);
         $statement->execute();
         $item = $statement->fetch();
         $statement->closeCursor();
